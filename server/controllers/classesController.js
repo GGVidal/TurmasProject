96c97eb362fcf61be87a7bb2436781775627339f
Gabriel Gondim <52431033+GGVidal@users.noreply.github.com>
2020-08-30 23:08:41 -0300

    Adding schema and routes validations (#3)

--- server/controllers/classesController.js
+++ server/controllers/classesController.js
@@ -14,9 +14,34 @@ class ClassesController {
         finish: yup.string().required(),
       }),
     });
-    const data = await Classes.create(req.body);
+    if (!(await schema.isValid)) {
+      return res.status(400).json({ error: "Validação falhou" });
+    }
 
-    return res.json(data);
+    const classExist = await Classes.find({
+      classroom: req.body.classroom,
+      instructors: req.body.instructors,
+      host: req.body.host,
+      account: req.body.account,
+      day: req.body.day,
+    });
+    if (classExist) {
+      return res.status(400).json({ error: "Sala já existe" });
+    }
+    try {
+      const {
+        classroom,
+        instructors,
+        host,
+        account,
+        day,
+        schedule,
+      } = await Classes.create(req.body);
+
+      return res.json({ classroom, instructors, host, account, day, schedule });
+    } catch (err) {
+      return res.status(401).json({ error: "Erro interno" });
+    }
   }
   async index(req, res) {
     const data = await Classes.find({});
