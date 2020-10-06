0dfaf1edf4b417332963a1ac769dffd5feb57057
GGVidal <gabrielvidalgondim@gmail.com>
2020-10-06 00:25:17 -0300

    CHG: Changing fields of json

--- server/models/classes.js
+++ server/models/classes.js
@@ -10,31 +10,57 @@ const scheduleSchema = new mongoose.Schema({
     required: true,
   },
 });
+const disciplinesSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  professor: {
+    type: String,
+    required: true,
+  },
+});
+const classInfoSchema = new mongoose.Schema({
+  day: {
+    type: String,
+    required: true,
+  },
+  disciplines: {
+    type: [disciplinesSchema],
+    required: true,
+  },
+});
 
+const zoomSchema = new mongoose.Schema({
+  room: {
+    type: String,
+    required: true,
+  },
+  host: {
+    type: String,
+    required: true,
+  },
+  account: {
+    type: String,
+    required: true,
+  },
+});
 const classes = new mongoose.Schema(
   {
-    classroom: {
+    name: {
       type: String,
       required: true,
     },
-    host: {
-      type: String,
+    class_info: {
+      type: classInfoSchema,
       required: true,
     },
-    account: {
-      type: String,
-      required: true,
-    },
-    day: {
-      type: String,
+    zoom: {
+      type: zoomSchema,
       required: true,
     },
     schedule: {
       type: scheduleSchema,
-      required: true,
-    },
-    instructors: {
-      type: Array,
       required: true,
     },
   },
